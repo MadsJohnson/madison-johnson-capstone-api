5c3a88d70c4c127d7b09318158f61d5172e4474c
Madison <madisonjohnson301@gmail.com>
2024-01-17 00:26:12 -0500

    find one todo by id

--- controllers/todo-controllers.js
+++ controllers/todo-controllers.js
@@ -11,6 +11,30 @@ const todo = (_req, res) => {
     });
 };
 
+// find todo with given id 
+const getTodoById = async (req, res) => {
+    knex("todo")
+    .where({task_id: req.params.task_id})
+    .then((todoFound)=> {
+        if (todoFound.length === 0) {
+            return res
+            .status(404)
+            .json({ message: `todo with ID: ${req.params.task_id} not found` });
+        }
+
+        const todoData = todoFound[0];
+        res.status(200).json(todoData);
+    })
+    .catch(() => {
+        res.status(500).json({
+          message: `Unable to retrieve todo data for todo with ID: ${req.params.task_id}`,
+        });
+      });
+    
+  };
+
+
 module.exports = {
   todo,
+  getTodoById
 };
--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const todoController = require("../controllers/todo-controllers.js");
 
-router.route("/").get(todoController.todo);
+router.route("/")
+    .get(todoController.todo);
+
+router.route("/:task_id")
+    .get(todoController.getTodoById);
 
 module.exports = router;
 
